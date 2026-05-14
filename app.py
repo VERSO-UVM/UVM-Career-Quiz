d5a66d71e023fff621bb8042f122d35740bb003a
Fauche-r <fauxcheur597@gmail.com>
2026-05-14 18:46:37 -0400

    We can finally have multiple quiz ! Will need to work on the css for that page (as well as TODO in app.py) .

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template,request, jsonify
+from flask import Flask, render_template,request
+import os
 import json
 
 app = Flask(__name__)
 
 
-# TODO change the route here from "/" to "/quiz builder" whenever we have a real landing page, right now it only exist as a way to actually test the flask 
-# TODO right now it only accept "testing_quiz/pjo4roy.json" as an argument, once we have quiz_selection done, we would wnat it to pass the name of the file to this, and open the file this way
-@app.route('/', methods=['GET', 'POST'])
-def quiz_builder():
-    quiz = {"title": "", "desc": "", "id": "", "categories": []}
+@app.route('/quiz_builder/<quiz>', methods=['GET', 'POST'])
+def quiz_builder(quiz):
+    quiz_ = {"title": "", "desc": "", "id": "", "categories": []}
     error = ""
     try:
-        with open('testing_quiz/testing_quiz/pjo4roy.json') as f:
-            quiz = json.load(f)
+        with open(f'testing_quiz/{quiz}') as f:
+            quiz_ = json.load(f)
     except FileNotFoundError as e:
         error = e 
-    return render_template('quiz_builder.html', quiz=quiz, error = error)
+    return render_template('quiz_builder.html', quiz =quiz_, error = error)
 
 
 
-# TODO change the test.json name to something that is unique (and would be unique to each file)
 @app.route('/save-quiz', methods=['POST'])
 def save_quiz():
     quiz = request.get_json()
     with open(f'testing_quiz/{quiz["id"]}.json', 'w') as f:
         json.dump(quiz, f, indent=2)
     return render_template('quiz_builder.html', quiz=quiz)
+
+
+# TODO this is good if we think the user is in a "single-player" state. We need to refactor this to have some more verification, but for now it will have to do. 
+@app.route("/", methods=['GET', 'POST'])
+def quiz_selection():
+    folder_path = './testing_quiz'
+    quiz = []
+    for filename in os.listdir(folder_path):
+        quiz.append(filename)
+    return render_template("quiz_selection.html", quizzes = quiz)
