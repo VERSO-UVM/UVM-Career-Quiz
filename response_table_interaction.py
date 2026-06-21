ae2cd44794ed8e8fb5049deb8724e75c46930294
plantluvr0 <ejorr@uvm.edu>
2026-06-21 17:49:10 -0400

    started lookup functionality for completed quizzes -- untested

--- response_table_interaction.py
+++ response_table_interaction.py
@@ -2,8 +2,30 @@ import json
 import sqlite3
 import csv
 import pickle
+#TODO: check sql queries to ensure correct data is being handled, make sure that any errors that occur for now cannot happen in prod, a lot of this can be dangerous if serial data can be mixed with unserialzed data and either re-serialized or joined wrong, all data types must be checked in every function. 
+#-----------------------------------WARNING!!!!!----------------------------------------------------
+
+
+
+
+
+# THIS FILE OPERATES ON CORE DATA THAT CANNOT BE DAMAGED IN ANY WAY AND WILL BE DIFFICULT TO RECOVER, ALTERING ANYTHING MUST BE PAIRED WITH TESTING OF ALL END BEHAVIORS OF RESULTING -
+# STATE. THIS PROGRAM CAN AND WILL LEAD TO UNINTENDED CONSEQUENSES ACROSS LARGE QUANTITIES OF SENSITIVE USER DATA THAT WILL PROVIDE INACCURATE RESULTS AND SYSTEM LEVEL ERRORS IF -
+# FUNCTIONS ARE ALTERED IN A WAY THAT DISRUPTS THE PROCESS THAT ALL FUNCTIONS MUST FOLLOW AS DETAILED BELOW.
+
+
+
+
+#ALL FUNCTIONS MUST BEHAVE IN THIS WAY: RETRIEVE DATA FROM DB IF NECESSARY, RETURN DATA FROM SERIALIZED FORM TO LIST FORM, PERFORM OPERATIONS NECESSARY TO BUSINESS LOGIC, CONSIDER BRANCHING BEHAVIORS AND DO NOT TRACK ANSWERS TO INDIVIDUAL QUIZZES ON A GLOBAL STATE AS THE VARIANCE CREATED THROUGH BRANCHING CAN CAUSE DIFFERENT QUIZ LENGTHS ALONG THE USER SPACE, WHEN DATA IS RETRIEVED OR ALTERRED BY USER SERIALZE THE DATA IF NECESSARY *(SEE STAR FOR SPECIFICATION ON THIS, WIP), RETURN DATA TO DATABASE MAKING SURE TO OVERWRITE EXISTING ENTRY AND NOT APPENDING (DUE TO THE NATURE OF BRANCHING A VARIABLE ADDRESS POINTER SPACE MUST BE USED WITHIN USER ROWS TO FIND QUIZ ANSWERS PROPERLY)
+
+
+# * DATA THAT CURRENTLY REQUIRES SERIALIZATION ARE ON TABLE "USER_RESPONSES" AND ARE COLUMNS: "quizzes_to_do", "quizzes_completed", "user_answers" THESE WILL BE EVENTUALLY WRAPPED IN A BEHAVIOR THAT DOES NOT ALLOW THEM TO EXIST IN THE INCORRECT STATE IN THE INCORRECT LOCATION BUT FOR NOW THIS MUST BE DONE MANUALLY. 
+
+
+
+
 """lookup table is gonna be in csv format for efficiency in python"""
-"""ROW: u_id, #completed_quizzes, quizzes_to_do[q_id], quizzes_completed[(q_id, len_quiz)], quiz_response_answers[(ques_id, answer)]"""
+"""ROW: u_id, #completed_quizzes, quizzes_to_do[q_id], quizzes_completed[(q_id, len_quiz)], user_answers[(ques_id, answer)]"""
 TEST_STRING = '''  {
  "userID": "TEST_ID",
  "quizID": "5d8256bb-d74c-46a6-9c97-30145f95b580",
@@ -193,7 +215,7 @@ def _incriment_quiz_ctr(u_id: str):
     conn.commit()
     conn.close()
 
-def _move_quiz_id_todo_cmp(u_id):
+def _move_quiz_id_todo_cmp(u_id, q_id, len_quiz):
     conn, cur = connecting_to_sql()
     query = """SELECT quizzes_to_do FROM USER_RESPONSES WHERE u_ID = ?"""
     #TODO: more
@@ -203,16 +225,16 @@ def _move_quiz_id_todo_cmp(u_id):
     query = """SELECT completed_quizzes FROM USER_RESPONSE WHERE u_ID = ?"""
     cmp = cur.execute(query, (u_id,))
     cmp = return_from_serial(cmp)
-    for i in range(len(cmp)):
-        for j in range(len(todo)):
-            if todo[j][0] == cmp[i][0]:
-                complete = todo[j].pop()
-                cmp.append(complete)
+    for i in range(len(todo)):
+        if todo[i][0] == q_id:
+            _ = todo[i].pop()
+            cmp.append((q_id, len_quiz))
     todo = serialize(todo)
     cmp = serialize(cmp)
     cur.execute("UPDATE USER_RESPONSE SET quizzes_to_do = ?, completed_quizzes = ? WHERE u_ID = ?", (todo, cmp, u_id))
     conn.commit()
     conn.close()
+
 def _append_answers(u_id, answer_list):
     conn,cur = connecting_to_sql()
     query = """SELECT user_answers FROM USER_RESPONSE WHERE u_ID = ?"""
@@ -225,21 +247,65 @@ def _append_answers(u_id, answer_list):
     conn.commit()
     conn.close()
 
-def quiz_complete():
-        pass
+def quiz_complete(json_string):
+       u_id, q_id, len_quiz, answer_arr = _json_data_convert(json_string)
+       _incriment_quiz_ctr(u_id)
+       _move_quiz_id_todo_cmp(u_id, q_id, len_quiz)
+       _append_answers(u_id, answer_arr)
+
 
     # -----------------HELPER FUNCTIONS------------------------
 def csv_lookup_to_dict():
+        # admin can lookup keywords and will provide questions that relate to that, then can be queried for. 
         pass
-def check_user_lookup_status():
+def check_user_lookup_status(admin_id, u_id):
+        # check to see if all users are accessible to admin, should kill the request to data 
         pass
+def data_type_correction_wrapper():
+    #this will eventually be a class for data but not yet just a placeholder see * above for more information
+    pass
     # -----------------LOOKUP BEHAVIORS------------------------
 def findall_users_cmp_quiz():
         pass
-def get_user_response_to_quiz():
-        pass
-def get_response_breakdown_on_users():
-        pass
+def get_user_response_to_quiz(u_id, q_id):
+    conn, cur = connecting_to_sql()
+    query = """SELECT num_completed_quizzes, completed_quizzes, user_answers FROM USER_RESPONSE WHERE u_ID = ?"""
+    cur.execute(query, (u_id,))
+    num_cmp_quiz, completed_quizzes, user_answers = cur.fetchone()
+    conn.close()
+    completed_quizzes = return_from_serial(completed_quizzes)
+    user_answers = return_from_serial(user_answers)
+    offset= 0
+    end= 0
+    for i in range(len(completed_quizzes)):
+        if completed_quizzes[i][0] == q_id:
+            offset= sum(completed_quizzes[:i-1][1])
+            end= offset + completed_quizzes[i][1]
+    curr_quiz_answers = user_answers[offset:end]
+    return curr_quiz_answers
+    
+def get_response_breakdown_on_users(ques_id, user_id_array, q_id):
+    conn, cur = connecting_to_sql()
+    responses = []
+
+    query = """SELECT completed_quizzes, user_answers FROM USER_RESPONSE WHERE u_ID IN ?"""
+    cur.execute(query, (user_id_array,))
+    user_comp_quizzes, user_answers = cur.fetchall()
+    conn.close()
+    user_comp_quizzes = return_from_serial(user_comp_quizzes)
+    user_answers = return_from_serial(user_answers)
+    for user in user_comp_quizzes:
+        for quiz in user:
+            if quiz[0] == q_id:
+                pass
+
+
+            
+        # loop through user checking
+        #1. is q_id in user completed quizzes
+        #2. if so, fetch the user responses to the quizzes (get_user_response_to_quiz)
+        #3. check to see if question id in list of responses, this can sometimes not happen because of branching
+        # append user_id, question_response to responses where u_id 
 def fetch_results_of_question_id_on_user_set():
         pass
 def lookup_kw_arg_on_user_set():
